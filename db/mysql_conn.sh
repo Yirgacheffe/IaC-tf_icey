d661f556d3defde2a00c4bbf7580ec2698f85e89
Aaron Yu <aaron.yuu@outlook.com>
2022-01-16 16:37:30 +0800

    use env value conn to rds.

--- db/mysql_conn.sh
+++ db/mysql_conn.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-RDS_HOST="mysql-icey-1.clkcufuco2l8.ap-southeast-1.rds.amazonaws.com"
 
-# O l e g # local1234
-mysql -h ${RDS_HOST} -P 3306 -u admin -p < $1
+# Connect to RDS according to the env value, execute sql scripts in $1 
+mysql -h ${DB_HOST} -P 3306 -u ${DB_USER} -p < $1
